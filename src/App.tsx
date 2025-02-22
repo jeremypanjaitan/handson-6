e9980cd32f8f1aac44fdf36d5701f80b6ea910a6
Jeremy Panjaitan <jeremy.panjaitan@nanovest.io>
2025-02-22 20:53:14 +0700

    Able to get list of product

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Carts from "./pages/Carts";
@@ -5,19 +6,22 @@ import Post from "./pages/Post";
 import Product from "./pages/Product";
 import Recipes from "./pages/Recipes";
 
+const queryClient = new QueryClient()
 
 function App() {
 	const router = createBrowserRouter(createRoutesFromElements(
-		<Route path="/" element={<RootLayout/>}>
-			<Route index element={<Product/>}/>	
-			<Route path="recipes" element={<Recipes/>}/>	
-			<Route path="posts" element={<Post/>}/>	
-			<Route path="carts" element={<Carts/>}/>	
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<Product />} />
+			<Route path="recipes" element={<Recipes />} />
+			<Route path="posts" element={<Post />} />
+			<Route path="carts" element={<Carts />} />
 		</Route>
 	));
 	return (
 		<>
-		<RouterProvider router={router}/>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
 		</>
 	)
 }
